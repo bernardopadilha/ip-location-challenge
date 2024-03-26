46cfeb76437744b2d5995dc765db44f93b1dc11d
Bernardo <bernardo_limas@hotmail.com>
2024-03-26 18:15:34 -0300

    feat: add info modal

--- src/App.tsx
+++ src/App.tsx
@@ -25,23 +25,22 @@ export function App() {
       <div className="bg-white rounded-xl flex flex-col md:flex-row items-center justify-center gap-5 md:gap-20 py-5 px-14 absolute top-[180px] md:top-[270px] z-[1000] left-1/2 -translate-x-1/2 ">
         <div className="flex flex-col items-start justify-center gap-2 md:gap-5">
           <span className="text-zinc-500/80">IP Adress</span>
-          <h1 className="text-xl font-bold">192.212.174.101</h1>
+          <h1 className="text-xl font-bold">{response ? response.ip : '192.212.174.101'}</h1>
         </div>
 
         <div className="flex flex-col items-start justify-center gap-2 md:gap-5 border-t md:border-t-0 md:border-l border-black pt-2 md:pt-0  md:pl-6">
           <span className="text-zinc-500/80">Location</span>
-          <h1 className="text-xl font-bold whitespace-nowrap">Brooklyn, NY 10001</h1>
+          <h1 className="text-xl font-bold whitespace-nowrap">{response ? `${response.location.city}, ${response.location.country}` : 'Santa Catarina, BR 10001'}</h1>
         </div>
 
         <div className="flex flex-col items-start justify-center gap-2 md:gap-5 border-t md:border-t-0 md:border-l border-black pt-2 md:pt-0 md:pl-6">
           <span className="text-zinc-500/80">Timezone</span>
-          <h1 className="text-xl font-bold whitespace-nowrap">UTC -05:00</h1>
+          <h1 className="text-xl font-bold whitespace-nowrap">{response ? response.location.timezone : 'UTC -03:00'}</h1>
         </div>
 
         <div className="flex flex-col items-start justify-center gap-2 md:gap-5 border-t md:border-t-0 md:border-l border-black pt-2 md:pt-0 md:pl-6">
           <span className="text-zinc-500/80">ISP</span>
-          <h1 className="text-xl font-bold whitespace-nowrap">SpaceX
-            StarLink</h1>
+          <h1 className="text-xl font-bold">{response ? response.isp : 'SpaceX StarLink'}</h1>
         </div>
 
 
